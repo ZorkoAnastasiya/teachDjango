39acef5f7c95c57229b0113b786d02bf42600682
ZorkoAnastasiya <asskarra@gmail.com>
2021-09-10 16:05:36 +0300

    start Django

--- Superproject/project/settings.py
+++ Superproject/project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h6az@9_$pkhi&58m7g70dia8%9(uo!odi8w#^jb$6ftn9^s@tj'
+SECRET_KEY = "1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
